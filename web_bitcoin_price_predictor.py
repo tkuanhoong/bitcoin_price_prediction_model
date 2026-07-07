6957ad189221980ba515103cbfbb423ccd16aff9
Tan Kuan Hoong <tkuanhoong@gmail.com>
2026-07-07 14:54:22 +0800

    fix: dependencie issue

--- web_bitcoin_price_predictor.py
+++ web_bitcoin_price_predictor.py
@@ -1,9 +1,9 @@
+import yfinance as yf
 import streamlit as st
 import pandas as pd
 import numpy as np
 from keras.models import load_model
 import matplotlib.pyplot as plt
-import yfinance as yf
 from matplotlib.ticker import ScalarFormatter
 
 st.title("BTC Price Prediction App")
